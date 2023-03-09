efaa69956617cac44a227eb848929a36a2918b8e
Senfelds <ralfs.senfelds@sk.lvt.lv>
2023-03-09 11:59:29 +0200

    Ieviesta iespeja mest kaulinu

--- src/Cirks.java
+++ src/Cirks.java
@@ -1,9 +1,11 @@
+import java.util.Random;
 
 public class Cirks {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int skaitlis;
+		Random rand = new Random();
+		skaitlis = rand.nextInt(6)+1;
+		System.out.println("Uzkrita skaitlis: "+skaitlis);
 	}
-
 }
