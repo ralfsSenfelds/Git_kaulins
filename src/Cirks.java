f374e9498e1abfd2499f6e2785316df845283ff5
Senfelds <ralfs.senfelds@sk.lvt.lv>
2023-03-09 12:59:42 +0200

    ielikt atstarpe

--- src/Cirks.java
+++ src/Cirks.java
@@ -4,17 +4,13 @@ import java.util.Scanner;
 public class Cirks {
 
 	public static void main(String[] args) {
-		int skaitlis, reizes, temp;
+		int skaitlis, reizes;
 		Scanner scan = new Scanner(System.in);
 		Random rand = new Random();
 		System.out.println("Cik reizes velies mest kaulinu?");
 		reizes = scan.nextInt();
 		for(int i=0; i<reizes; i++){
 			skaitlis = rand.nextInt(6)+1;
-			temp = skaitlis;
-			if(temp == skaitlis){
-				skaitlis = rand.nextInt(6)+1;
-			}
 			System.out.println("Uzkrita skaitlis: "+skaitlis);
 		}
 		scan.close();
