a87257383551a25768cab27110be30e08e831e48
Ralfs Šenfelds <ralfsssenfelds@gmail.com>
2023-04-05 09:59:40 +0300

    Parada lielako skaitli kurs tika uzmests

--- src/gitKaulins.java
+++ src/gitKaulins.java
@@ -3,20 +3,25 @@ import java.util.Scanner;
 
 public class gitKaulins {
 	static void mestKaulinu(int reizes){
-		int skaitlis;
+		int skaitlis, lielakais=0;
 		Random rand = new Random();
 		
 		for(int i=0; i<reizes; i++){
 			skaitlis = rand.nextInt(6)+1;
 			System.out.println("Uzkrita skaitlis: "+skaitlis);
+			if(lielakais<skaitlis)
+				lielakais = skaitlis;
 		}
+		System.out.println("Lielakais skaitlis bija: "+lielakais);
 	}
 	
 	public static void main(String[] args) {
 		int reizes;
 		Scanner scan = new Scanner(System.in);
+		do {
 		System.out.println("Cik reizes mest kaulinu?");
 		reizes = scan.nextInt();
+		}while(reizes<1);
 		mestKaulinu(reizes);
 		scan.close();
 	}
